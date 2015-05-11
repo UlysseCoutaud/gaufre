bd99d8faeaf46e77202306a4329e6aedcb4977bd
UlysseCoutaud <luly_30@hotmail.fr>
2015-05-11 15:28:18 +0200

    le joueur dumb est implement, un test l'accompagne

--- src/player/Dumb.java
+++ src/player/Dumb.java
@@ -5,14 +5,28 @@ import java.util.Random;
 
 public class Dumb implements Player {
 
+
+	/*
+	 * (non-Javadoc)
+	 * @see player.Player#makeChoice(Board)
+	 */
 	@Override
-	public Point makeChoice(Board currentConfig) {
+	public Point makeChoice(BoardUlysse currentConfig) {
 		Random r = new Random();
-		int tirage = r.nextInt(currentConfig.width*currentConfig.height);
-		
-		
-		return new Point(0,0);
+		int tirage = r.nextInt(currentConfig.width*currentConfig.height)+1;
+		int i=0, j=0;
+
+		while(tirage>0){
+			i = (i+1) % currentConfig.width ;
+			if ( i==0 ){
+				j = (j+1) % currentConfig.height ;
+			}
+			if( BoardUlysse.isWaffle(i,j) || BoardUlysse.isPoison(i,j) ){
+				tirage--;
+			}
+		}
+		return new Point(i,j);
 	}
-	
+
 }
 
