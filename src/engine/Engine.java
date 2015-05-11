5dd1d54eb1c2c83fb4626b272f831dabb1afae36
Valysaar <valpruliere@gmail.com>
2015-05-11 20:50:05 +0000

    fusion de Data et Engine en Engine, et fusion de Board et GameState en GameState, ajout de squelette pour les autres classes

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -2,14 +2,68 @@ package engine;
 
 import ihm.GuiController;
 
+import java.awt.Point;
+import java.util.Stack;
+
+import util.util;
+
 public class Engine {
+	// pastStack contains present state as well
+	private Stack<GameState> pastStack;
+	private Stack<GameState> futureStack;
+	private GameState currentState;
+
+	int currentPlayer = 1;
 
 	public Engine(int boardWidth, int boardHeight, int nbOfHumanPlayers) {
 
+		pastStack = new Stack<GameState>();
+		futureStack = new Stack<GameState>();
 	}
 
 	public void setIHM(GuiController ihm) {
 
 	}
 
+	public void undoAction() {
+		if (!isUndoable()) {
+			util.debug("No Actions left to undo.");
+			return;
+		}
+		futureStack.push(currentState);
+		currentState = pastStack.pop();
+	}
+
+	public void redoAction() {
+		if (!isRedoable()) {
+			util.debug("No actions to redo.");
+			return;
+		}
+		pastStack.push(currentState);
+		currentState = futureStack.pop();
+	}
+
+	public void play(int x, int y) {
+		GameState currentState = getCurrentGameState(); // TODO
+	}
+
+	public void play(Point p) {
+		play(p.x, p.y);
+	}
+
+	public GameState getCurrentGameState() {
+		return currentState;
+	}
+
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public boolean isUndoable() {
+		return !pastStack.isEmpty();
+	}
+
+	public boolean isRedoable() {
+		return !futureStack.isEmpty();
+	}
 }
--- src/engine/GameState.java
+++ src/engine/GameState.java
@@ -1,25 +1,169 @@
 package engine;
 
+import java.awt.Point;
+
 public class GameState {
 
-	public enum Square {
+	public enum Cell {
 		EATEN, POISON, WAFFLE
 	}
 
-	Board board;
+	final public Cell[][] board;
+	final public int currentPlayer;
+	public final int width, height;
 
-	int currentPlayer;
+	private Cell[][] copyOf(Cell[][] board) {
+		Cell[][] res = new Cell[width][height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				res[i][j] = board[i][j];
+			}
+		}
+		return res;
+	}
 
-	public GameState(Board board, int currentPlayer) {
-		this.board = board.cloneBoard();
+	public GameState(Cell[][] board, int width, int height, int currentPlayer) {
+		this.board = copyOf(board);
 		this.currentPlayer = currentPlayer;
+		this.width = width;
+		this.height = height;
 	}
 
 	public int getCurrentPlayer() {
 		return this.currentPlayer;
 	}
 
-	public Board getBoard() {
-		return board;
+	public GameState cloneState() {
+		return null; // TODO
+	}
+
+	public class EatableIterator {
+
+		private int x, y;
+		private int nextX, nextY;
+
+		private EatableIterator() {
+			x = 0;
+			y = 0;
+		}
+
+		boolean hasNext() {
+			for (int i = x; i < width; i++) {
+				for (int j = y; j < height; j++) {
+					if (isWaffle(i, j)) {
+						nextX = i;
+						nextY = j;
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		Cell next() {
+			x = nextX;
+			y = nextY;
+			return board[x][y];
+		}
+
+		int getX() {
+			return x;
+		}
+
+		int getY() {
+			return y;
+		}
+
+		Point getPoint() {
+			return new Point(x, y);
+		}
+	}
+
+	public EatableIterator getEatableIterator() {
+		return new EatableIterator();
+	}
+
+	public boolean isWaffle(int i, int j) {
+		return board[i][j] == Cell.WAFFLE;
+	}
+
+	public boolean isWaffle(Point p) {
+		return isWaffle(p.x, p.y);
+	}
+
+	public boolean isEaten(int i, int j) {
+		return board[i][j] == Cell.EATEN;
+	}
+
+	public boolean isEaten(Point p) {
+		return isEaten(p.x, p.y);
+	}
+
+	public boolean isPoison(int i, int j) {
+		return board[i][j] == Cell.POISON;
+	}
+
+	public boolean isPoison(Point p) {
+		return isPoison(p.x, p.y);
+	}
+
+	/**
+	 * Returns true if we can eat this square without being poisoned
+	 */
+	public boolean isSafe(int i, int j) {
+		return i != 0 || j != 0;
+	}
+
+	@Override
+	public String toString() {
+		String string = new String();
+		for (int i = 0; i < width; i++) {
+			string += "+---";
+		}
+		string += "+\n";
+		for (int i = 0; i < width; i++) {
+			string += "| ";
+			for (int j = 0; j < height; j++) {
+				if (isPoison(i, j)) {
+					string += "P ";
+				} else if (isWaffle(i, j)) {
+					string += "W ";
+				} else {
+					string += "  ";
+				}
+				string += "| ";
+			}
+			string += "\n";
+			for (int k = 0; k < width; k++) {
+				string += "+---";
+			}
+			string += "+\n";
+		}
+		return string;
+	}
+
+	/**
+	 * Eats all the waffle squares to the right and under p
+	 */
+	public void eat(Point p) {
+		for (int i = p.x; i < width; i++) {
+			for (int j = p.y; j < height; j++) {
+				remove(i, j);
+			}
+		}
+	}
+
+	/**
+	 * Removes one waffle square
+	 */
+	private void remove(int i, int j) {
+		board[i][j] = Cell.EATEN;
+	}
+
+	/**
+	 * Removes one waffle square
+	 */
+	public void remove(Point p) {
+		board[p.x][p.y] = Cell.EATEN;
 	}
 }
