c8f0ae710c51762b3d1b0aa57a4bf70436fddfd4
Valysaar <valpruliere@gmail.com>
2015-05-12 10:53:07 +0000

    "merge" complete

--- src/engine/GameState.java
+++ src/engine/GameState.java
@@ -4,8 +4,6 @@ import java.awt.Point;
 
 public class GameState {
 
-	// Properties
-
 	public enum Cell {
 		EATEN, POISON, WAFFLE
 	}
@@ -13,8 +11,6 @@ public class GameState {
 	public final Cell[][] board;
 	public int currentPlayer;
 	public final int width, height;
-
-	// Constructors
 
 	public GameState(Cell[][] board, int currentPlayer) {
 		this.board = this.copyBoard(board);
@@ -33,8 +29,6 @@ public class GameState {
 	public int getCurrentPlayer() {
 		return this.currentPlayer;
 	}
-
-	// State of cells
 
 	/**
 	 * Returns true if the only square left is poisoned
@@ -120,8 +114,6 @@ public class GameState {
 		return str;
 	}
 
-	// Board helpers
-
 	private Cell[][] generateNewBoard(int w, int h) {
 		Cell[][] res = new Cell[w][h];
 		for (int i = 0; i < w; i++) {
@@ -175,8 +167,6 @@ public class GameState {
 		}
 		return string;
 	}
-
-	// Iterators
 
 	public EdibleIterator getEdibleIterator() {
 		return new EdibleIterator();
--- src/util/Logger.java
+++ src/util/Logger.java
@@ -1,39 +1,38 @@
-
 package util;
 
 public class Logger {
 
-    // Properties
+	// Properties
 
-    public static boolean enableAppLog = true;
-    public static boolean enableEngineLog = true;
-    public static boolean enableGuiLog = true;
-    public static boolean enableIaLog = true;
+	public static boolean enableAppLog = true;
+	public static boolean enableEngineLog = true;
+	public static boolean enableGuiLog = true;
+	public static boolean enableIaLog = true;
 
-    // Logs
+	// Logs
 
-    public static void logApp(String str) {
-        if (Logger.enableAppLog) {
-            System.out.println(str);
-        }
-    }
+	public static void logApp(String str) {
+		if (Logger.enableAppLog) {
+			System.out.println(str);
+		}
+	}
 
-    public static void logEngine(String str) {
-        if (Logger.enableEngineLog) {
-            System.out.println(str);
-        }
-    }
+	public static void logEngine(String str) {
+		if (Logger.enableEngineLog) {
+			System.out.println("ENGINE DEBUG : " + str);
+		}
+	}
 
-    public static void logGui(String str) {
-        if (Logger.enableGuiLog) {
-            System.out.println(str);
-        }
-    }
+	public static void logGui(String str) {
+		if (Logger.enableGuiLog) {
+			System.out.println("GUI DEBUG : " + str);
+		}
+	}
 
-    public static void logIa(String str) {
-        if (Logger.enableIaLog) {
-            System.out.println(str);
-        }
-    }
+	public static void logIa(String str) {
+		if (Logger.enableIaLog) {
+			System.out.println("IA DEBUG : " + str);
+		}
+	}
 
 }
